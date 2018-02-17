9fd6ffe446ca988e2b88fb9d724eadbff0880a22
ggljzr <gogo.lejzr@gmail.com>
2018-02-17 11:01:57 +0100

    removed unused atributes of Base class

--- GarageSystem/app/mod_main/models.py
+++ GarageSystem/app/mod_main/models.py
@@ -5,10 +5,6 @@ class Base(db.Model):
     __abstract__ = True
 
     id = db.Column(db.Integer, primary_key=True)
-    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
-    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
-                              onupdate=db.func.current_timestamp())
-
 
 class Garage(Base):
     __tablename__ = 'garage'
