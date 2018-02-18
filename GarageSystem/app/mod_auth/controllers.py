4fbeefd9bf27ff89f5b2dbe3d8b57738c480507d
ggljzr <gogo.lejzr@gmail.com>
2018-02-18 16:15:34 +0100

    added flash messages for logging in and out

--- GarageSystem/app/mod_auth/controllers.py
+++ GarageSystem/app/mod_auth/controllers.py
@@ -7,10 +7,16 @@ mod_auth = Blueprint('auth', __name__)
 @mod_auth.route('/login', methods=['POST'])
 def login():
     pw_man = PasswordManager()
-    session['logged_in'] = pw_man.check_password(request.form['password'])
+    if pw_man.check_password(request.form['password']):
+        session['logged_in'] = True
+    else:
+        flash('Neplatné heslo', 'error')
+
     return redirect('/')
+
 
 @mod_auth.route('/logout')
 def logout():
     session['logged_in'] = False
+    flash('Odhlášení proběhlo úspěšně')
     return redirect('/')
