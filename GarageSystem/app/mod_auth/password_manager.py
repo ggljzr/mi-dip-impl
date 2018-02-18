0aaffd4a4b6c39c9453b26b208fca3edfe0eb970
ggljzr <gogo.lejzr@gmail.com>
2018-02-18 16:06:19 +0100

    password storing with bcrypt

--- GarageSystem/app/mod_auth/password_manager.py
+++ GarageSystem/app/mod_auth/password_manager.py
@@ -9,10 +9,18 @@ class PasswordManager():
         self.user_config.read(USER_CONFIG_PATH)
 
     def check_password(self, password):
-        if password == self.user_config['settings']['password']:
-            return True
+        pw_encoded = password.encode('utf-8')
+        pw_hash = self.user_config['settings']['password'].encode('utf-8')
 
+        if bcrypt.checkpw(pw_encoded, pw_hash):
+           return True
+        
         return False
 
-    def save_password(self, password):
-        pass
+    def save_password(self, new_password):
+        pw_encoded = new_password.encode('utf-8')
+        pw_hash = bcrypt.hashpw(pw_encoded, bcrypt.gensalt())
+        self.user_config['settings']['password'] = pw_hash.decode('utf-8')
+
+        with open(USER_CONFIG_PATH, 'w') as f:
+            self.user_config.write(f)
