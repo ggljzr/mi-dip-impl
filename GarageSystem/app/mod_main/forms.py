ee6197f36ade7072a552fa7302ae27c70a0963a9
ggljzr <gogo.lejzr@gmail.com>
2018-02-18 17:18:55 +0100

    reformating

--- GarageSystem/app/mod_main/forms.py
+++ GarageSystem/app/mod_main/forms.py
@@ -2,11 +2,15 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField
 from wtforms.validators import NumberRange
 
+
 class GarageForm(FlaskForm):
     tag = StringField('Označení')
-    period = IntegerField('Perioda', validators=[NumberRange(1, 999, message="Perioda musí být mezi 1 a 999")])
+    period = IntegerField('Perioda', validators=[NumberRange(
+        1, 999, message="Perioda musí být mezi 1 a 999")])
+
 
 class GarageFormBuilder:
+
     def build_form(garage):
         garage_form = GarageForm()
         garage_form.tag.data = garage.tag
