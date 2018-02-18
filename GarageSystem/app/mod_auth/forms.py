e9fac9a7ac8fb50ffb0c1b157fbb0c36c06eabe2
ggljzr <gogo.lejzr@gmail.com>
2018-02-18 17:18:25 +0100

    change password form validation

--- GarageSystem/app/mod_auth/forms.py
+++ GarageSystem/app/mod_auth/forms.py
@@ -1,10 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import PasswordField
+from wtforms.validators import Length, EqualTo
+
 
 class LoginForm(FlaskForm):
     password = PasswordField('Heslo')
 
+
 class ChangePasswordForm(FlaskForm):
     old_password = PasswordField('Staré heslo')
-    new_password = PasswordField('Nové heslo')
-    repeat_password = PasswordField('Nové heslo znovu')
+    new_password = PasswordField('Nové heslo', validators=[
+        Length(min=8, message='Heslo musí mít alespoň 8 znaků')])
+    repeat_password = PasswordField('Nové heslo znovu', validators=[
+        EqualTo('new_password', message='Hesla musí být stejná')])
