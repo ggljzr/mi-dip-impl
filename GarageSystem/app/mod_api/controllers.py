5dbb858d4e5d6a5a007d103139c47cae3c1b2658
ggljzr <gogo.lejzr@gmail.com>
2018-02-20 22:46:45 +0100

    very basic event logging

--- GarageSystem/app/mod_api/controllers.py
+++ GarageSystem/app/mod_api/controllers.py
@@ -1,13 +1,20 @@
-from flask import Blueprint
+from flask import Blueprint, request
 
-from app.mod_main.models import Model
+from app.mod_main.models import Model, InvalidAPIKeyError
 
 mod_api = Blueprint('api', __name__)
 
 @mod_api.route('/api/add_garage')
 def add_garage():
-    pass
+    return 'ok', 200
 
-@mod_api.route('/api/add_event', methods=['POST'])
-def add_event():
-    pass
+@mod_api.route('/api/add_report_event', methods=['POST'])
+def add_report_event():
+    api_key = request.headers.get('Authorization')
+
+    try: 
+        Model.add_report_event(api_key)
+    except InvalidAPIKeyError:
+        return 'not ok', 403
+    
+    return 'ok', 200
--- GarageSystem/app/mod_main/models.py
+++ GarageSystem/app/mod_main/models.py
@@ -2,13 +2,14 @@ from app import db
 
 import uuid
 
+
 class Base(db.Model):
     __abstract__ = True
 
     id = db.Column(db.Integer, primary_key=True)
 
+
 class Garage(Base):
-    __tablename__ = 'garage'
     tag = db.Column(db.String(128), default='Nová garáž')
     api_key = db.Column(db.String(32), default=None)
     last_report = db.Column(db.DateTime, default=None)
@@ -16,19 +17,29 @@ class Garage(Base):
     period = db.Column(db.Integer, default=60)
     state = db.Column(db.SmallInteger, default=0)
 
+    events = db.relationship('Event', backref='Garage', lazy=True, enable_typechecks=False)
+
     def __init__(self):
-        self.tag = 'Nová garáž'
         self.api_key = uuid.uuid4().hex
-        self.last_report = None
-        self.next_report = None
-        self.period = 60
-        self.state = 0
 
     def __repr__(self):
         return 'Garáž[{}]: {}'.format(self.id, self.tag)
 
 
+class Event(Base):
+    timestamp = db.Column(db.DateTime, default=db.func.current_timestamp())
+
+    garage_id = db.Column(db.Integer, db.ForeignKey(
+        'garage.id'), nullable=False)
+
+class ReportEvent(Event):
+    next_report = db.Column(db.DateTime, default=None)
+
+class InvalidAPIKeyError(Exception):
+    pass
+
 class Model:
+
     def add_garage():
         new_garage = Garage()
         db.session.add(new_garage)
@@ -51,4 +62,14 @@ class Model:
 
         if garage != None:
             garage.api_key = uuid.uuid4().hex
+            db.session.commit()
+
+    def add_report_event(api_key):
+        garage = Garage.query.filter_by(api_key=api_key).first()
+
+        if garage == None:
+            raise InvalidAPIKeyError
+        else:
+            event = ReportEvent(garage_id=garage.id)
+            garage.events.append(event)
             db.session.commit()
