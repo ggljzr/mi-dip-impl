cd374809a52ec2972dce73d3e860736374662690
ggljzr <gogo.lejzr@gmail.com>
2018-02-27 22:36:51 +0100

    note about disabling csrf

--- GarageSystem/app/__init__.py
+++ GarageSystem/app/__init__.py
@@ -20,6 +20,9 @@ from app.mod_auth.controllers import mod_auth as auth_module
 from app.mod_main.controllers import mod_main as main_module
 from app.mod_api.controllers import mod_api as api_module
 
+#api module routes does not require login,
+#so no csrf is required 
+#(hijacking session is useless, autentization is done via api key)
 csrf.exempt(api_module)
 
 app.register_blueprint(main_module)
