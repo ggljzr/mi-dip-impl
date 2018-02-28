ea0bafcd4522d8b141f5dd10db0f448a1de72d0d
ggljzr <gogo.lejzr@gmail.com>
2018-02-28 04:01:07 +0100

    period unit in ui

--- GarageSystem/app/mod_main/forms.py
+++ GarageSystem/app/mod_main/forms.py
@@ -5,7 +5,7 @@ from wtforms.validators import NumberRange
 
 class GarageForm(FlaskForm):
     tag = StringField('Označení')
-    period = IntegerField('Perioda', validators=[NumberRange(
+    period = IntegerField('Perioda (minuty)', validators=[NumberRange(
         1, 999, message="Perioda musí být mezi 1 a 999")])
     note = TextAreaField('Poznámka')
 
