dea5be4c0314d5264088cb34d067240cf9bdbb86
ggljzr <gogo.lejzr@gmail.com>
2018-03-04 22:59:00 +0100

    some notes about api

--- GarageSystem/app/mod_api/controllers.py
+++ GarageSystem/app/mod_api/controllers.py
@@ -4,6 +4,15 @@ from app.mod_main.models import Garage
 
 mod_api = Blueprint('api', __name__)
 
+#tady to udelat proste /garages a /events a post
+#pro vytvareni novejch eventu a garazi
+#jak je to popsany v 
+#https://docs.microsoft.com/en-us/azure/architecture/best-practices/api-design
+
+#u tech eventu to udelat proste tak ze typ eventu je soucasti toho
+#pozadavku, tj je posilanej tim postem, tzn bude jen jedna routa
+#pro vsechny eventy
+
 @mod_api.route('/api/add_garage')
 def add_garage():
     return 'ok', 200
