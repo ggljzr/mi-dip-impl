8caa247263f737d0e3042a837893b4ff0084574f
ggljzr <gogo.lejzr@gmail.com>
2018-03-07 12:57:22 +0100

    moved filters implementation from controller to separate class

--- GarageSystem/app/mod_main/controllers.py
+++ GarageSystem/app/mod_main/controllers.py
@@ -3,8 +3,8 @@ from flask import Blueprint, render_template, request, redirect, url_for, flash,
 from jinja2 import Markup
 
 from .models.garage import Garage
-from .models.event import Event
 from .forms import GarageFormBuilder, GarageForm
+from .filters import Filters
 
 from app.mod_auth.auth_utils import login_required
 
@@ -13,55 +13,23 @@ mod_main = Blueprint('main', __name__)
 
 @mod_main.app_template_filter('date_filter')
 def date_filter(date):
-    if date == None:
-        return 'Žádné'
+    return Filters.date_filter(date)
 
-    return date
 
 @mod_main.app_template_filter('garage_doors_filter')
 def garage_doors_filter(doors):
-    if doors == Garage.DOORS_OPEN:
-        return 'Otevřeno'
+    return Filters.garage_doors_filter(doors)
 
-    if doors == Garage.DOORS_CLOSED:
-        return 'Zavřeno'
-
-    return 'Nedefinováno'
 
 @mod_main.app_template_filter('garage_state_filter')
 def garage_state_filter(state):
-    if state == Garage.STATE_OK:
-        return 'OK'
+    return Filters.garage_state_filter(state)
 
-    if state == Garage.STATE_NOT_RESPONDING:
-        return 'Nehlásí se'
-
-    if state == Garage.STATE_SMOKE:
-        return 'Detekce kouře'
-
-    if state == Garage.STATE_MOVEMENT:
-        return 'Detekce pohybu'
-
-    return 'Nedefinováno'
 
 @mod_main.app_template_filter('event_filter')
 def event_filter(event):
-    ret = '[{}]'.format(event.timestamp)
+    return Filters.event_filter(event)
 
-    if event.type == Event.TYPE_REPORT:
-        ret = ret + ' Kontrolní událost'
-    elif event.type == Event.TYPE_DOOR_OPEN:
-        ret = ret + ' Otevření dveří'
-    elif event.type == Event.TYPE_DOOR_CLOSED:
-        ret = ret + ' Zavření dveří'
-    elif event.type == Event.TYPE_MOVEMENT:
-        ret = ret + ' Detekce pohybu!'
-    elif event.type == Event.TYPE_SMOKE:
-        ret = ret + ' Detekce kouře!'
-    else:
-        ret = ret + 'Nedefinováno'
-
-    return ret
 
 @mod_main.route('/')
 @login_required
---
+++ GarageSystem/app/mod_main/filters.py
@@ -0,0 +1,51 @@
+from .models.event import Event
+from .models.garage import Garage
+
+class Filters():
+    def date_filter(date):
+        if date == None:
+            return 'Žádné'
+
+        return date
+
+    def garage_doors_filter(doors):
+        if doors == Garage.DOORS_OPEN:
+            return 'Otevřeno'
+
+        if doors == Garage.DOORS_CLOSED:
+            return 'Zavřeno'
+
+        return 'Nedefinováno'
+
+    def garage_state_filter(state):
+        if state == Garage.STATE_OK:
+            return 'OK'
+
+        if state == Garage.STATE_NOT_RESPONDING:
+            return 'Nehlásí se'
+
+        if state == Garage.STATE_SMOKE:
+            return 'Detekce kouře'
+
+        if state == Garage.STATE_MOVEMENT:
+            return 'Detekce pohybu'
+
+        return 'Nedefinováno'
+        
+    def event_filter(event):
+        ret = '[{}]'.format(event.timestamp)
+
+        if event.type == Event.TYPE_REPORT:
+            ret = ret + ' Kontrolní událost'
+        elif event.type == Event.TYPE_DOOR_OPEN:
+            ret = ret + ' Otevření dveří'
+        elif event.type == Event.TYPE_DOOR_CLOSED:
+            ret = ret + ' Zavření dveří'
+        elif event.type == Event.TYPE_MOVEMENT:
+            ret = ret + ' Detekce pohybu!'
+        elif event.type == Event.TYPE_SMOKE:
+            ret = ret + ' Detekce kouře!'
+        else:
+            ret = ret + 'Nedefinováno'
+
+        return ret
