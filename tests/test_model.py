eb89e3c5dccb4c6be935fc71d2f231887c74d06c
ggljzr <gogo.lejzr@gmail.com>
2018-03-22 13:10:44 +0100

    garage fixture, test_add_garage

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,5 +1,25 @@
 import pytest
-from garage_system.mod_main.models.garage import Garage, Event
+import os
 
-def test_basic():
-    print('ok')
+# test tips : http://alexmic.net/flask-sqlalchemy-pytest/
+#           : http://flask.pocoo.org/docs/0.12/testing/#testing
+
+import testing_config
+
+@pytest.fixture
+def garage():
+    # set app config to testing via env var
+    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+    os.environ['GARAGE_SYSTEM_CONFIG'] = BASE_DIR + '/testing_config.py'
+
+    # initialize app with testing config
+    from garage_system.mod_main.models.garage import Garage
+    yield Garage
+
+    # teardown (delete db)
+    os.unlink(BASE_DIR + '/test_app.db')
+
+def test_add_garage(garage):
+    garage.add_garage()
+    # adds exactly one garage
+    assert len(garage.query.all()) == 0
