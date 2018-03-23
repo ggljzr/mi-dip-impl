44516cf4a33f77c8660ff168c9b4b6009fbbd7d0
ggljzr <gogo.lejzr@gmail.com>
2018-03-23 08:07:23 +0100

    check_report test

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,6 +1,7 @@
 import pytest
 import os
-
+from datetime import datetime
+from freezegun import freeze_time
 # test tips : http://alexmic.net/flask-sqlalchemy-pytest/
 #           : http://flask.pocoo.org/docs/0.12/testing/#testing
 #           : https://docs.pytest.org/en/latest/fixture.html#fixture-finalization-executing-teardown-code (yield thing)
@@ -32,3 +33,17 @@ def test_revoke_key(garage):
     new_garage.revoke_key()
 
     assert old_key != new_garage.api_key
+
+@freeze_time("2011-01-01 00:00:00")
+def test_check_report(garage):
+    new_garage = garage.add_garage()
+    new_garage.period = 60 # explicitly set period
+    new_garage.add_report_event()
+    new_garage.check_report()
+
+    assert new_garage.state == garage.STATE_OK
+
+    # move time forward two hours so report is missed
+    with freeze_time("2011-01-01 02:00:00"):
+        new_garage.check_report()
+        assert new_garage.state == garage.STATE_NOT_RESPONDING
