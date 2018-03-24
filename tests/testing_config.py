79c3260b7da2decf2057fb76dad97fa0b7269f82
ggljzr <gogo.lejzr@gmail.com>
2018-03-24 13:53:30 +0100

    disabled csrf for testing

--- tests/testing_config.py
+++ tests/testing_config.py
@@ -11,7 +11,10 @@ DATABASE_CONNECT_OPTIONS = {}
 
 THREADS_PER_PAGE = 2
 
-CSRF_SECRET_KEY = os.urandom(16)
+# csrf disabled for testing
+WTF_CSRF_ENABLE = False
+WTF_CSRF_CHECK_DEFAULT = False
+
 SECRET_KEY = os.urandom(16)
 
 USER_CONFIG_PATH = os.path.join(BASE_DIR, 'test_user_config.ini')
