c7ec8d162b12ac24facf4decc7d03ecf81f16d02
ggljzr <gogo.lejzr@gmail.com>
2018-03-24 14:48:41 +0100

    login_required now returns 403 status code when when user is not logged in

--- garage_system/mod_auth/auth_utils.py
+++ garage_system/mod_auth/auth_utils.py
@@ -7,7 +7,7 @@ def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if not session.get('logged_in'):
-            return redirect('/login')
+            return redirect('/login'), 403
         return f(*args, **kwargs)
 
     return decorated_function
