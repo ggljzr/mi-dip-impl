479e50b500fc7d92900aa521f371a78e7e62c8e6
ggljzr <gogo.lejzr@gmail.com>
2018-03-26 14:41:35 +0200

    added functions for testing index and garage view

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,7 @@
 import pytest
-import os
+
+import testing_utils
+from flask import session
 
 """
 main controller unit tests
@@ -7,7 +9,7 @@ main controller unit tests
 this also test application views (templates)
 """
 
-@pytest.fixture(scope='module') # teardown after last test in module
+@pytest.fixture() # teardown after last test in module
 def app_client():
     # set app config to testing via env var
     testing_utils.setup()
@@ -21,7 +23,51 @@ def app_client():
     test_garage = Garage.add_garage()
 
     from garage_system import app
+    # set logged in flag to be true for all tests
     yield app.test_client()
 
     # teardown (delete db)
     testing_utils.teardown()
+
+def test_index(app_client):
+    testing_utils.set_logged_in(app_client)
+    response = app_client.get('/')
+    # we get main page right away
+    # (no redirects)
+    assert response.status == '200 OK'
+    assert 'garages_box' in response.data.decode('utf-8')
+
+def test_garage(app_client):
+    testing_utils.set_logged_in(app_client)
+
+    # get first garage
+    response = app_client.get('/garage/1')
+
+    assert response.status == '200 OK'
+    assert 'show_garage_container' in response.data.decode('utf-8')
+
+def test_nonexistent_garage(app_client):
+    testing_utils.set_logged_in(app_client)
+
+    response = app_client.get('/garage/468864684')
+
+    # get 404
+    assert response.status == '404 NOT FOUND'
+
+def test_edit_garage(app_client):
+    testing_utils.set_logged_in(app_client)
+
+    # edit first garage data
+    response = app_client.post('/garage/1', data={
+        'tag' : 'some testing tag',
+        'period' : 60,
+        'note' : 'some testing note'
+        })
+
+    response_data = response.data.decode('utf-8')
+
+    assert response.status == '200 OK'
+    assert 'some testing tag' in response_data
+    assert 'some testing note' in response_data
+
+
