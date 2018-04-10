762aa03564e41674c73b7c5140f1d22110f240ff
ggljzr <gogo.lejzr@gmail.com>
2018-04-10 03:36:31 +0200

    refactored sms_send

--- garage_system/mod_main/sms_utils.py
+++ garage_system/mod_main/sms_utils.py
@@ -2,12 +2,19 @@ import subprocess
 import unicodedata
 
 def send_sms(phone, text):
-    if phone is not None:
-        # try to send sms if gammu daemon is installed
-        # recipe to change non ascii chars https://stackoverflow.com/a/2701901
-        ascii_txt = unicodedata.normalize('NFKD', text).encode('ascii','ignore')
-        try:
-            subprocess.call(['gammu-smsd-inject', 'TEXT',
-                             phone, '-text', ascii_txt])
-        except FileNotFoundError:
-            pass
+    if phone is None:
+        return False
+
+    if phone is '':
+        return False
+
+    # try to send sms if gammu daemon is installed
+    # recipe to change non ascii chars https://stackoverflow.com/a/2701901
+    ascii_txt = unicodedata.normalize('NFKD', text).encode('ascii','ignore')
+    try:
+        subprocess.call(['gammu-smsd-inject', 'TEXT',
+                         phone, '-text', ascii_txt])
+    except FileNotFoundError:
+        return False
+
+    return True
