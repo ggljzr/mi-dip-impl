af7d4e9a702c00600129066b3bf8128cb6458a86
ggljzr <gogo.lejzr@gmail.com>
2018-04-10 02:56:39 +0200

    added unicode encoding

--- garage_system/mod_main/sms_utils.py
+++ garage_system/mod_main/sms_utils.py
@@ -3,8 +3,9 @@ import subprocess
 def send_sms(phone, text):
     if phone is not None:
         # try to send sms if gammu daemon is installed
+        uni = text.encode('utf-8')
         try:
             subprocess.call(['gammu-smsd-inject', 'TEXT',
-                             phone, '-unicode', '-text', text])
+                             phone, '-unicode', '-text', uni])
         except FileNotFoundError:
             pass
