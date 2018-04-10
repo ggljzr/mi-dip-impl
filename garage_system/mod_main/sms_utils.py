b9168d5fe85d1befdc2f29c5e93dc7455d4d6b95
ggljzr <gogo.lejzr@gmail.com>
2018-04-10 03:41:38 +0200

    added clear_unicode parameter with default=True to send_sms

--- garage_system/mod_main/sms_utils.py
+++ garage_system/mod_main/sms_utils.py
@@ -1,7 +1,7 @@
 import subprocess
 import unicodedata
 
-def send_sms(phone, text):
+def send_sms(phone, text, clear_unicode=True):
     if phone is None:
         return False
 
@@ -10,7 +10,11 @@ def send_sms(phone, text):
 
     # try to send sms if gammu daemon is installed
     # recipe to change non ascii chars https://stackoverflow.com/a/2701901
-    ascii_txt = unicodedata.normalize('NFKD', text).encode('ascii','ignore')
+    ascii_txt = text
+
+    if clear_unicode == True:
+        ascii_txt = unicodedata.normalize('NFKD', text).encode('ascii','ignore')
+    
     try:
         subprocess.call(['gammu-smsd-inject', 'TEXT',
                          phone, '-text', ascii_txt])
