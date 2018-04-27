8e7d2756697366e30f40f05567ac21e8e966cf45
ggljzr <gogo.lejzr@gmail.com>
2018-04-27 03:54:30 +0200

    commented out sqlalchemy_echo option in default config

--- default_config.py
+++ default_config.py
@@ -7,6 +7,7 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+# SQLALCHEMY_ECHO=True
 DATABASE_CONNECT_OPTIONS = {}
 
 THREADS_PER_PAGE = 2
